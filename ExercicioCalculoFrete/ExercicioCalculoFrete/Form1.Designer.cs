700bbc30960bd61c031a89724e9659997d548b09
Érick <104468867+3r1ckamorim@users.noreply.github.com>
2022-07-28 22:04:25 -0300

    Add files via upload

---
+++ ExercicioCalculoFrete/ExercicioCalculoFrete/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ExercicioCalculoFrete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.cbxUf = new System.Windows.Forms.ComboBox();
+            this.CalcularFrete = new System.Windows.Forms.Button();
+            this.LimparCampos = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblValorCompra = new System.Windows.Forms.Label();
+            this.lblFrete = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.Controls.Add(this.LimparCampos);
+            this.groupBox1.Controls.Add(this.CalcularFrete);
+            this.groupBox1.Controls.Add(this.cbxUf);
+            this.groupBox1.Controls.Add(this.txtValor);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(320, 205);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Dados de Entrega";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblTotal);
+            this.groupBox2.Controls.Add(this.lblFrete);
+            this.groupBox2.Controls.Add(this.lblValorCompra);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(13, 250);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(320, 149);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Valores Calculados";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Estado";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(60, 19);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(186, 20);
+            this.txtNome.TabIndex = 3;
+            this.txtNome.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(60, 62);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(100, 20);
+            this.txtValor.TabIndex = 4;
+            // 
+            // cbxUf
+            // 
+            this.cbxUf.FormattingEnabled = true;
+            this.cbxUf.Items.AddRange(new object[] {
+            "SP",
+            "RJ",
+            "AM",
+            ""});
+            this.cbxUf.Location = new System.Drawing.Point(60, 104);
+            this.cbxUf.Name = "cbxUf";
+            this.cbxUf.Size = new System.Drawing.Size(100, 21);
+            this.cbxUf.TabIndex = 5;
+            // 
+            // CalcularFrete
+            // 
+            this.CalcularFrete.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.CalcularFrete.Location = new System.Drawing.Point(10, 153);
+            this.CalcularFrete.Name = "CalcularFrete";
+            this.CalcularFrete.Size = new System.Drawing.Size(113, 45);
+            this.CalcularFrete.TabIndex = 6;
+            this.CalcularFrete.Text = "Calcular";
+            this.CalcularFrete.UseVisualStyleBackColor = false;
+            this.CalcularFrete.Click += new System.EventHandler(this.CalcularFrete_Click);
+            // 
+            // LimparCampos
+            // 
+            this.LimparCampos.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.LimparCampos.Location = new System.Drawing.Point(161, 153);
+            this.LimparCampos.Name = "LimparCampos";
+            this.LimparCampos.Size = new System.Drawing.Size(115, 45);
+            this.LimparCampos.TabIndex = 7;
+            this.LimparCampos.Text = "Limpar";
+            this.LimparCampos.UseVisualStyleBackColor = false;
+            this.LimparCampos.Click += new System.EventHandler(this.LimparCampos_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Valor da Compra:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Frete:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 102);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Total:";
+            // 
+            // lblValorCompra
+            // 
+            this.lblValorCompra.AutoSize = true;
+            this.lblValorCompra.Location = new System.Drawing.Point(111, 38);
+            this.lblValorCompra.Name = "lblValorCompra";
+            this.lblValorCompra.Size = new System.Drawing.Size(49, 13);
+            this.lblValorCompra.TabIndex = 3;
+            this.lblValorCompra.Text = "_______";
+            // 
+            // lblFrete
+            // 
+            this.lblFrete.AutoSize = true;
+            this.lblFrete.Location = new System.Drawing.Point(111, 71);
+            this.lblFrete.Name = "lblFrete";
+            this.lblFrete.Size = new System.Drawing.Size(49, 13);
+            this.lblFrete.TabIndex = 4;
+            this.lblFrete.Text = "_______";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(111, 102);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(49, 13);
+            this.lblTotal.TabIndex = 5;
+            this.lblTotal.Text = "_______";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(345, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Cálculo de entrega";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cbxUf;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button LimparCampos;
+        private System.Windows.Forms.Button CalcularFrete;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblFrete;
+        private System.Windows.Forms.Label lblValorCompra;
+    }
+}
+
---
+++ ExercicioCalculoFrete/ExercicioCalculoFrete/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExercicioCalculoFrete
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalcularFrete_Click(object sender, EventArgs e)
+        {
+            var frete = 0.0;
+            var total = 0.0;
+            var valor = 0.0;
+            string uf = cbxUf.Text;
+
+            valor = double.Parse(txtValor.Text);
+
+            if(valor > 1000.0)
+            {
+                frete = 0;
+            }
+
+            else if (uf == "AM")
+            {
+                frete = 20.0;
+            }
+
+            else if (uf == "SP")
+            {
+                frete = 5.0;
+            }
+            else if (uf == "RJ")
+            {
+                frete = 10.0;
+            }
+
+            else if( uf!= "SP" && uf != "RJ" && uf != "AM")
+            {
+                frete = 15.0;
+            }
+
+            total = valor + frete;
+
+            lblValorCompra.Text = valor.ToString("C");
+            lblFrete.Text = frete.ToString("C");
+            lblTotal.Text = total.ToString("C");
+
+        }
+
+        private void LimparCampos_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtValor.Text = String.Empty;
+            cbxUf.Text = String.Empty;
+        }
+
+
+    }
+}
